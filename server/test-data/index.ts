38cc88991d1d9245873412f8bca7e74de76ed548
ErrolKeith <errol.k.keith@gmail.com>
2025-04-01 13:22:28 -0400

    add defaults

---
+++ server/test-data/index.ts
@@ -0,0 +1,10 @@
+import type { Globals } from "../types";
+
+const globalDefaults: Globals = {
+  siteId: "[unique-site-uuid]",
+  companyName: "[Company Name]",
+  phoneNumber: "[Phone Number]",
+  services: ["[product-1]", "[product-2]"],
+};
+
+export { globalDefaults };
